e096bf6bbbda326d33ab62370984581699814514
Haider Lasani <haiderlasani@gmail.com>
2024-11-04 08:45:30 +0100

    Creates Municipality

---
+++ app/Modules/WmoSubsidy/App/Models/Municipality.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Modules\WmoSubsidy\App\Models;
+
+use Database\Factories\MunicipalityFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Municipality extends Model
+{
+    /** @use HasFactory<MunicipalityFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name'];
+}
---
+++ database/factories/MunicipalityFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Modules\WmoSubsidy\App\Models\Municipality;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Municipality>
+ */
+class MunicipalityFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->city,
+        ];
+    }
+}
