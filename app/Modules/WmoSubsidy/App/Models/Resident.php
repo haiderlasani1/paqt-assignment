6fab17bc58e5f1552d001070d9cb301523016b91
Haider Lasani <haiderlasani@gmail.com>
2024-11-04 09:01:34 +0100

    Creates Resident

---
+++ app/Modules/WmoSubsidy/App/Models/Resident.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\WmoSubsidy\App\Models;
+
+use Database\Factories\ResidentFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resident extends Model
+{
+    /** @use HasFactory<ResidentFactory> */
+    use HasFactory;
+}
